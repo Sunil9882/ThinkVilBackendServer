8253626ca533eb406165d8a5d19ab13d0c6f8139
Sunil Kumar <sunilkumarbakoliya123@gmail.com>
2025-03-23 11:05:49 +0530

    cors issue fix

--- src/main/java/com/Twitter/Jarvis/Config/WebConfig.java
+++ src/main/java/com/Twitter/Jarvis/Config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.Twitter.Jarvis.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,12 +8,39 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url.local}")
+    private String localfrontendURL;
+
+    @Value("${frontend.url.prod}")
+    private String prodFrontendUrl;
+
+    @Value("${frontend.url.dev}")
+    private String devFrontendUrl;
+
+    @Value("${spring.profiles.active}")
+    private String environment;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","https://jarviscom-jarvis-projects-f309a14c.vercel.app") // Replace with your frontend's URL
+                .allowedOrigins(getFrontendURL()) // Replace with your frontend's URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    public String getFrontendURL() {
+        String frontendUrl;
+        if(environment.equals("prod")) {
+            frontendUrl = prodFrontendUrl;
+        }
+        else if(environment.equals("dev")) {
+            frontendUrl = devFrontendUrl;
+        }
+        else {
+            frontendUrl = localfrontendURL;
+        }
+
+        return frontendUrl;
     }
 }
